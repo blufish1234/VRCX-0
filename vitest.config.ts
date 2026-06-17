9f5f97c35f30257343689e98e140160332e28031
Map1en <maplenagisa@gmail.com>
2026-06-17 22:35:13 +0900

    add test coverage and rust coverage badge

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
         }
     },
     test: {
-        environment: 'node'
+        environment: 'node',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json-summary'],
+            reportsDirectory: './coverage'
+        }
     }
 });
