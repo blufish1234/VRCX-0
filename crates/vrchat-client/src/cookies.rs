7f41bce33a664e6ff73a5129c68fe140b33f4743
Map1en <maplenagisa@gmail.com>
2026-07-01 23:06:07 +0900

    update copyright and licensing information in cookies.rs (#253)

--- crates/vrchat-client/src/cookies.rs
+++ crates/vrchat-client/src/cookies.rs
@@ -1,5 +1,30 @@
-// Modified from https://github.com/pfernie/reqwest_cookie_store/blob/c906d634bad35b9f79abdc36f939979e39d1a2e1/src/lib.rs
-// Original code licensed under MIT
+// Portions of this file are derived from reqwest_cookie_store
+// (https://github.com/pfernie/reqwest_cookie_store), commit
+// c906d634bad35b9f79abdc36f939979e39d1a2e1, src/lib.rs.
+//
+// Copyright (c) 2017 Patrick Fernie <patrick.fernie@gmail.com>
+//
+// The original work is dual-licensed under either the MIT License or the Apache
+// License, Version 2.0, at your option. It is used here under the MIT License,
+// reproduced below.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 use base64::engine::general_purpose::STANDARD as B64;
 use base64::Engine;
